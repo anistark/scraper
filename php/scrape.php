844894fb6903fda07c4d457506050eabee8354cb
Kumar Anirudha <anirudhastark@yahoo.com>
2014-10-06 12:45:40 +0530

    json stored

--- php/scrape.php
+++ php/scrape.php
@@ -1,19 +1,24 @@
 <?php
 include_once('simple_html_dom.php');
 
+$count = 0;
+
 function scraping_stackoverflow() {
     // create HTML DOM
-    $url='http://stackoverflow.com/questions/tagged/';
-    $tag=$_POST['value'];
-    $html = file_get_html($url.$tag);
+    $url='http://stackoverflow.com/questions/tagged/'.$_POST['value'];
+    $html = file_get_html($url);
 
     // get questions block
     foreach($html->find('div[class^=question-summary]') as $questions) {
         // get question url
         $item['question'] = trim($questions->find('a.question-hyperlink', 0)->plaintext);
+        // get question vote
+        $item['vote'] = trim($questions->find('span.vote-count-post', 0)->plaintext);
         // get status
         $item['status'] = trim($questions->find('div.status', 0)->plaintext);
-
+        // get views
+        $item['views'] = trim($questions->find('div.views', 0)->plaintext);
+        
         $ret[] = $item;
     }
     
@@ -31,10 +36,13 @@ $ret = scraping_stackoverflow();
 foreach($ret as $v) {
     echo $v['questions'].'<br>';
     echo '<ul>';
-    echo '<li>'.$v['question'].'<strong> Status: </strong>'.$v['status'].'</li>';
+    echo '<li>'.$v['question'].'<strong> Votes: </strong>'.$v['vote'].'<strong> Status: </strong>'.$v['status'].'<strong> Viewed: </strong>'.$v['views'].'</li>';
     echo '</ul>';
+    if ($v['status']=='0answers')
+        $count = $count+1;
 }
 
+// json part
 $file = file_get_contents('data.json');
 $data = json_decode($file);
 unset($file);//prevent memory leaks for large json.
@@ -43,5 +51,15 @@ $data[] = array('data'=>'scraper data');
 //save the file
 file_put_contents('data.json',json_encode($ret));
 unset($data);//release memory
+
+$file = file_get_contents('data.json');
+$json_array  = json_decode($file, true);
+unset($file);
+$elementCount  = count($json_array);
+
+// Display
+$answered_count = $elementCount - $count;
+echo 'Total questions: '.$elementCount.'<br />';
+echo 'Total Answered questions: '.$answered_count;
 
 ?>
